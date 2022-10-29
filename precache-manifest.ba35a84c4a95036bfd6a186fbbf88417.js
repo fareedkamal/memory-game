efb9a762a5241c453f49aeb7f108f337abb8cbad
fareedkamal <fkamalars@gmail.com>
2022-10-29 23:58:20 +0500

    Updates

---
+++ precache-manifest.ba35a84c4a95036bfd6a186fbbf88417.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "220d902b23904156b027",
+    "url": "/memory-game/static/css/main.be019843.chunk.css"
+  },
+  {
+    "revision": "220d902b23904156b027",
+    "url": "/memory-game/static/js/main.76d12a20.chunk.js"
+  },
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  },
+  {
+    "revision": "96334e75cf6b7d208937",
+    "url": "/memory-game/static/js/2.e8c6d4a4.chunk.js"
+  },
+  {
+    "revision": "e45ebd5258d77392cb78ceb7b3133eeb",
+    "url": "/memory-game/static/media/github-logo.e45ebd52.png"
+  },
+  {
+    "revision": "ada31f53ff98ba4a94e63c3faeeb66ea",
+    "url": "/memory-game/static/media/img (1).ada31f53.jpg"
+  },
+  {
+    "revision": "817d290f42ce8928439ea97d6e609e8e",
+    "url": "/memory-game/static/media/img (10).817d290f.jpg"
+  },
+  {
+    "revision": "f49303e517aaa504a9408a20ad12b095",
+    "url": "/memory-game/static/media/img (11).f49303e5.jpg"
+  },
+  {
+    "revision": "bf995a1d301bbe24b2cbec61800432d8",
+    "url": "/memory-game/static/media/img (12).bf995a1d.jpg"
+  },
+  {
+    "revision": "f11a7873f84d95b834d678bde670f485",
+    "url": "/memory-game/static/media/img (13).f11a7873.jpg"
+  },
+  {
+    "revision": "ac183f7d75592928ff52968cd14d8fef",
+    "url": "/memory-game/static/media/img (14).ac183f7d.jpg"
+  },
+  {
+    "revision": "17a1ccf2aab1200ef0494b04c316189d",
+    "url": "/memory-game/static/media/img (15).17a1ccf2.jpg"
+  },
+  {
+    "revision": "9b0ae1af5102477338ac82cf733bb1de",
+    "url": "/memory-game/static/media/img (2).9b0ae1af.jpg"
+  },
+  {
+    "revision": "533cbb1c2ed23f84098e3cb0ad8bc094",
+    "url": "/memory-game/static/media/img (3).533cbb1c.jpg"
+  },
+  {
+    "revision": "2b80bd21f26380444de3d1cbfde9fb03",
+    "url": "/memory-game/static/media/img (4).2b80bd21.jpg"
+  },
+  {
+    "revision": "45bb874fa81f5008b57ec2d8f9b7b5c9",
+    "url": "/memory-game/static/media/img (5).45bb874f.jpg"
+  },
+  {
+    "revision": "e33b7e0274a78ae88c2397e96ec23e0a",
+    "url": "/memory-game/static/media/img (6).e33b7e02.jpg"
+  },
+  {
+    "revision": "ed895988dc794dd2d2925efe825c7198",
+    "url": "/memory-game/static/media/img (7).ed895988.jpg"
+  },
+  {
+    "revision": "992b64ba2f9c8c7d4507b978d3b25e99",
+    "url": "/memory-game/static/media/img (8).992b64ba.jpg"
+  },
+  {
+    "revision": "fdec6e42bfd70c01180321f943539eea",
+    "url": "/memory-game/static/media/img (9).fdec6e42.jpg"
+  },
+  {
+    "revision": "8a9aed102d2028d93156502aa45909ed",
+    "url": "/memory-game/index.html"
+  }
+];
